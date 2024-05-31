14f74f97d903ea366b5ac5553a48053dac7119ef
Joanna Adermark <joanna.adermark@tutanota.com>
2024-05-31 07:02:51 +0200

    add uilsearch and uillocation to inputs

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -7,9 +7,25 @@ import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
         <div className="flex flex-row w-3/4 justify-center space-x-4">
             <input 
             type="text" 
-            placeholder="Search...."
-            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize" />
+            placeholder="Search for city...."
+            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize" 
+            />
+        <UilSearch 
+         size="25" 
+         className="text-white cursor-pointer transition ease-out hover:scale-125" 
+            />
+        <UilLocationPoint 
+         size="25" 
+         className="text-white cursor-pointer transition ease-out hover:scale-125" />
         </div>
+
+        <div className="flex flex-row w-1/4 items-center justify-center space-x-2">
+        <button className="text-xl text-white font-light">°C</button>
+        <span className="text-xl text-white">|</span>
+        <button className="text-xl text-white font-light">°F</button>
+      </div>
+
+
     </div>
   );
 }
